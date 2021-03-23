b0af6dfb403cfdb8290bfa49b1575a8c58391aee
Lenophie <quasiphies@gmail.com>
2021-03-23 11:59:25 +0100

    Add séance 3 progress

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,23 +1,35 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     [SerializeField] private float speed = 10f;
-    [SerializeField] private Transform playerTransform;
+    [SerializeField] private float jumpForce = 1000f;
+    [SerializeField] private Rigidbody2D playerRigidbody;
     [SerializeField] private Animator playerAnimator;
+
+    private bool hasJumped;
 
     // Start is called before the first frame update
     private void Start() {
+        hasJumped = false;
         playerAnimator.SetTrigger("StartMovement");
     }
 
     // Update is called once per frame
     private void Update()
     {
-        playerTransform.position += speed * Input.GetAxis("Horizontal")
-                                          * new Vector3(Time.deltaTime, 0f, 0f);
+        if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0.3f)
+            playerRigidbody.AddForce(speed * Input.GetAxis("Horizontal")
+                                           * new Vector2(Time.deltaTime, 0f));
 
-        if (Input.GetButtonDown("MonNouvelAxe")) {
-            Debug.Log("test");
+        if (Input.GetButtonUp("Jump") && hasJumped == false) {
+            playerRigidbody.AddForce(jumpForce * new Vector2(0f, 1f));
+            hasJumped = true;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (Vector3.Dot(other.contacts[0].normal, new Vector2(0f, 1f)) > 0.8f)
+            hasJumped = false;
     }
 }
